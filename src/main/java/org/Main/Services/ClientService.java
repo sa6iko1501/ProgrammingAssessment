3c5feaca23f77d40d14dd4c13d6e7203bf72ca90
sa6iko1501 <aleksandstoianov@tu-sofia.bg>
2023-03-24 17:59:29 +0200

    1.01

--- src/main/java/org/Main/Services/ClientService.java
+++ src/main/java/org/Main/Services/ClientService.java
@@ -9,19 +9,14 @@ import java.util.List;
 
 public class ClientService {
     private Client client;
-    private ProductService productService = new ProductService();
+    private final ProductService productService = new ProductService();
     public void initiateClient(Client client, int[] products, List<Product> productList){
         this.client = client;
         this.client.setProducts(productService.initializeProductList(products,productList));
         calculateClientDiscount(this.client.getBasicClientDiscount());
 
     }
-    public double calculateProductPrices(){
-        for(int i=0;i<client.getProducts().size();i++){
 
-        }
-        return 0;
-    }
     public void calculateClientDiscount(double percentage){
         if(percentage!=0){
             percentage = percentage/100;
